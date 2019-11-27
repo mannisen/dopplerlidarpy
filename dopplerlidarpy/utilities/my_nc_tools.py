Bug fixes and improving the turbulence spectra calculation.

dopplerlidarpy/utilities/my_nc_tools.py
@@ -5,21 +5,38 @@ Created on Thu Jun 13 13:48:23 2019
 
 @author: manninan
 """
-import uuid
+
 from netCDF4 import Dataset
 from datetime import datetime
+import numpy as np
 from dopplerlidarpy.utilities import dl_toolbox_version
 import getpass
+import uuid
 
 
-def write_nc_(date_txt, file_name, obs, add_gats=None, title_=None, institution_=None, location_=None, source_=None):
+def create_nc_dims(rootgrp, var, dimension_names):
+    if var.dim_name is not None:
+        for dname, dsize in zip(var.dim_name, var.dim_size):
+            if dname not in dimension_names:
+                print(dname)
+                dimension_names.append(dname)
+                rootgrp.createDimension(dname, dsize)
+    return rootgrp, dimension_names
+
+
+def create_nc_var(rootgrp, var):
+    ncvar = rootgrp.createVariable(var.name, var.data_type, var.dim_name)
+    return rootgrp, ncvar
+
+
+def write_nc_(date_txt, file_name, obs, additional_gatts=None, title_=None, institution_=None, location_=None, source_=None):
     """Writes a netCDF file with name and full path specified by 'file_name' with variables as listed by 'obs'.
 
     Args:
         date_txt (str):
         file_name (str):
         obs (list):
-        add_gats (dict):
+        additional_gatts (dict):
         title_ (str):
         institution_ (str):
         location_ (str):
@@ -27,32 +44,37 @@ def write_nc_(date_txt, file_name, obs, add_gats=None, title_=None, institution_
 
     """
 
-    rootgrp = Dataset(file_name, 'w', format='NETCDF4_CLASSIC')
+    rootgrp = Dataset(file_name, 'w', format='NETCDF4')
 
     dimension_names = []
     for var in obs:
-        print("..variable '{}'".format(var.name))
         if var.dim_name is not None:
-            for i in range(len(var.dim_name)):
-                if not var.dim_name[i] in dimension_names:
-                    dimension_names.append(var.dim_name[i])
-                    rootgrp.createDimension(var.dim_name[i], var.dim_size[i])
-    
-        # Create netcdf variable
-        if var.dim_name is None:  # dimensionless
-            ncvar = rootgrp.createVariable(var.name,
-                                           var.data_type,
-                                           zlib=var.zlib,
-                                           fill_value=var.fill_value)
-        else:
-            ncvar = rootgrp.createVariable(var.name,
-                                           var.data_type,
-                                           var.dim_name,
-                                           zlib=var.zlib,
-                                           fill_value=var.fill_value)
+            for dname, dsize in zip(var.dim_name, var.dim_size):
+                if dname not in dimension_names:
+                    print(dname)
+                    dimension_names.append(dname)
+                    rootgrp.createDimension(dname, dsize)
+        ncvar = rootgrp.createVariable(var.name, var.data_type, var.dim_name)
 
-        ncvar[:] = var.data
+        # rootgrp, dimension_names = create_nc_dims(rootgrp, var, dimension_names)
+        # rootgrp, ncvar = create_nc_var(rootgrp, var)
+
+        # Create netcdf variable
+        #print("..variable '{}'".format(var.name))
+        # if var.dim_name is not None:
+        #     for dname, dsize in zip(var.dim_name, var.dim_size):
+        #         if dname not in dimension_names:
+        #             dimension_names.append(dname)
+        #             rootgrp.createDimension(dname, dsize)
+        #     ncvar = rootgrp.createVariable(var.name, var.data_type, var.dim_name)
+        # else:  # dimensionless
+        #     ncvar = rootgrp.createVariable(var.name, var.data_type)
+        # print("ncvar {}".format(np.shape(ncvar)))
+        # print("var.data {}".format(np.shape(var.data)))
+
+        ncvar[:] = var.data[:]
         ncvar.long_name = var.long_name
+
         if var.units:
             ncvar.units = var.units
         if var.error_variable:
@@ -77,9 +99,9 @@ def write_nc_(date_txt, file_name, obs, add_gats=None, title_=None, institution_
     rootgrp.institution = institution_
     rootgrp.location = location_
     rootgrp.source = source_
-    rootgrp.year = int(date_txt[0:4])
-    rootgrp.month = int(date_txt[4:6])
-    rootgrp.day = int(date_txt[6:])
+    rootgrp.year = int(date_txt[:4])
+    rootgrp.month = int(date_txt[5:7])
+    rootgrp.day = int(date_txt[8:10])
     rootgrp.software_version = dl_toolbox_version.__version__
 #    rootgrp.git_version = git_version()
     rootgrp.file_uuid = str(uuid.uuid4().hex)
@@ -89,7 +111,8 @@ def write_nc_(date_txt, file_name, obs, add_gats=None, title_=None, institution_
     history_msg = "NETCDF4 file created by user {} on {} UTC.".format(user_name, now_time_utc)
     rootgrp.history = history_msg
     # Additional global attributes
-    for key_, value_ in zip(add_gats.keys(), add_gats.values()):
-        setattr(rootgrp, key_, value_)
+    if additional_gatts is not None:
+        for key_, value_ in zip(additional_gatts.keys(), additional_gatts.values()):
+            setattr(rootgrp, key_, value_)
     rootgrp.close()
     print(history_msg)